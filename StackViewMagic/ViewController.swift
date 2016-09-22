e7fdb96d4110568fc3e9aed07e0de201d91089d0
Rickey Hrabowskie <rickeyhrabowskie@Rickeys-MacBook-Pro.local>
2016-09-22 12:22:00 -0500

    first commit

--- StackViewMagic/ViewController.swift
+++ StackViewMagic/ViewController.swift
@@ -10,16 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var emojiView: [UIView]! {
+        didSet {
+            emojiView.forEach { // Collection of UIViews
+                $0.isHidden = true
+            }
+        }
+    }
+    
+    @IBOutlet weak var emojiText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pizzaBtnPressed(_ sender: AnyObject) {
+        hideButtons()
+        emojiText.text = "🍕"
     }
-
+    @IBAction func worldBtnPressed(_ sender: AnyObject) {
+        hideButtons()
+        emojiText.text = "🌎"
+    }
+    @IBAction func gameBtnPressed(_ sender: AnyObject) {
+        hideButtons()
+        emojiText.text = "👾"
+    }
+    
+    @IBAction func hotBtnPressed(_ sender: AnyObject) {
+        hideButtons()
+        emojiText.text = "🔥"
+    }
+    
+    @IBAction func coffeeBtnPressed(_ sender: AnyObject) {
+        hideButtons()
+        emojiText.text = "☕️"
+    }
+    
+    @IBAction func rocketBtnPressed(_ sender: AnyObject) {
+        hideButtons()
+        emojiText.text = "🚀"
+    }
+    
+    @IBAction func tacoBtnPressed(_ sender: AnyObject) {
+        hideButtons()
+        emojiText.text = "🍕"
+    }
+    
+    @IBAction func onSettingsButtonPressed(_ sender: AnyObject) {
+        hideButtons()
+        emojiText.text = "🌮"
+    }
+    
+    func hideButtons() {
+        UIView.animate(withDuration: 0.3) { // Inside of a closure always use self
+            self.emojiView.forEach {
+                $0.isHidden = !$0.isHidden
+            }
+        }
+    }
 
 }
 
